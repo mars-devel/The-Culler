44eafb5b165972ab49a36636ceb0e42e9276ba2e
Wolfarmoon <wolfarmoon@proton.me>
2026-09-09 00:56:36 -0500

    1. Added the aditional logic to determine if the thread is in a forum channel that belongs to a list of channels that need moderation. 2. Refactored names and added some comments.

--- events/on_thread_creation.js
+++ events/on_thread_creation.js
@@ -1,4 +1,4 @@
-const { ThreadChannel, ActionRowBuilder, Events, MediaGalleryBuilder, MediaGalleryItemBuilder, TextDisplayBuilder, SeparatorBuilder, SeparatorSpacingSize, ButtonBuilder, ButtonStyle, SectionBuilder, ContainerBuilder, MessageFlags, ComponentType } = require('discord.js');
+const { ThreadChannel, ActionRowBuilder, Events, MediaGalleryBuilder, MediaGalleryItemBuilder, TextDisplayBuilder, SeparatorBuilder, SeparatorSpacingSize, ButtonBuilder, ButtonStyle, SectionBuilder, ContainerBuilder, MessageFlags, ComponentType, ChannelType, GuildMember, Role } = require('discord.js');
 
 const config = require("../config.json");
 
@@ -7,11 +7,21 @@ module.exports = {
 
     /** @param {ThreadChannel} thread */
     async execute(thread) {
-        const ownerid = thread.ownerId;
-        const user = (await thread.client.users.fetch(ownerid)).username;
-        const now = Date.now()
-        const fiveMinsFromNow = Math.floor(now / 1000) + 5 * 60;
 
+        const thread_owner_id = thread.ownerId;
+        const thread_owner_user = await thread.guild.members.fetch(thread_owner_id);
+        const thread_owner_name = thread_owner_user.displayName;
+
+        // Checks if the Thread originates from a MODERATED FORUM channel.
+        if (thread.parent.type != ChannelType.GuildForum) return;
+        if (thread.parent.id != config.moderated_forum_channels) return;
+
+        if (thread_owner_user.roles.cache.hasAny(...config.roles.override_thread_verification)) return;
+
+        const now = Date.now();
+        const in_5mins = Math.floor(now / 1000) + 5 * 60;
+        
+        // TODO: The messages on this component generator are provisional. We'll need to change them.
         function thread_message(description, footer) {
             return new ContainerBuilder()
                 .setAccentColor(31352)
@@ -23,7 +33,7 @@ module.exports = {
                         ),
                 )
                 .addTextDisplayComponents(
-                    new TextDisplayBuilder().setContent(`**You have created a thread, <@${ownerid}>**`),
+                    new TextDisplayBuilder().setContent(`**You have created a thread, <@${thread_owner_id}>**`),
                 )
                 .addSeparatorComponents(
                     new SeparatorBuilder().setSpacing(SeparatorSpacingSize.Small).setDivider(true),
@@ -67,23 +77,27 @@ module.exports = {
 
         }
 
-        const new_thread_msg = thread_message("By creating a thread, you acknowledge **you have read and understood the Wiki**. If that's not the case, DO NOT proceed.", `:warning: This thread is locked, and will be deleted on <t:${fiveMinsFromNow}:F> (<t:${fiveMinsFromNow}:R>), unless you click the button below.`)
+        const new_thread_msg = thread_message("By creating a thread, you acknowledge **you have read and understood the Wiki**. If that's not the case, DO NOT proceed.", `:warning: This thread is locked, and will be deleted on <t:${in_5mins}:F> (<t:${in_5mins}:R>), unless you click the button below.`);
 
-        const buttonid = ownerid + "." + now.toString() + ".ihaveread_confirm";
+        const button_id = thread_owner_id + "." + now.toString() + ".ihaveread_confirm";
 
-        // console.debug(buttonid)
-        // console.debug(buttonid.length)
+        // console.debug(button_id)
+        // console.debug(button_id.length)
 
-        const ihaveread = new ButtonBuilder().setCustomId(buttonid).setLabel('I HAVE read the wiki and I want to proceed.').setStyle(ButtonStyle.Secondary);
 
-        const row = new ActionRowBuilder().addComponents(ihaveread);
+        // Confirmation Button
+        const reading_confirmation_button = new ButtonBuilder().setCustomId(button_id).setLabel('I HAVE read the wiki and I want to proceed.').setStyle(ButtonStyle.Secondary);
+        const reading_confirmation_row = new ActionRowBuilder().addComponents(reading_confirmation_button);
 
+        // Sends the first message.
         const message = await thread.send({
-            components: [new_thread_msg, row],
+            components: [new_thread_msg, reading_confirmation_row],
             flags: MessageFlags.IsComponentsV2,
         });
 
-        thread.setLocked(true, `Thread is held locked until ${user} (${ownerid}) acknowledges the wiki thingie.`)
+        thread.setLocked(true, `Thread is held locked until ${thread_owner_name} (${thread_owner_id}) acknowledges the wiki thingie.`)
+
+        // Button interaction collector section
 
         const collector = message.createMessageComponentCollector({
             time: 5 * 60 * 1000,
@@ -91,25 +105,26 @@ module.exports = {
         });
 
         collector.on('collect', async interaction => {
-            if (interaction.customId == buttonid) {
+            if (interaction.customId == button_id) {
                 collector.stop('OK');
             }
         })
 
         collector.on('end', async (collected, reason) => {
-            console.log(`Collected ${collected.size} interactions.`);
+
+            // console.debug(`Collected ${collected.size} interactions.`);
             if (reason == "OK") {
-
+                thread.setLocked(false);
                 const ok_thread_msg = thread_message("You acknowledge **you have read and understood the Wiki**. If that's not the case, DO NOT proceed.", `Please, use your brain... :pleading_face:`)
-
                 message.edit(
                     { components: [ok_thread_msg], flags: MessageFlags.IsComponentsV2 | MessageFlags.Urgent }
                 )
                 return;
             }
-            await thread.delete(`${user} (${ownerid}) is dumb`);
-            console.debug(`Thread ${thread.name} has been deleted.`)
+            await thread.delete(`${thread_owner_name} (${thread_owner_user}) is dumb`);
+            // console.debug(`Thread ${thread.name} has been deleted.`)
         });
 
     }
 }
+
